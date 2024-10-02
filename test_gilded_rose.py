ff7d080439ed3997abb156765385de0ba3520992
Kevin990001 <79775196+Kevin990001@users.noreply.github.com>
2024-10-01 20:39:33 -0700

    add 3 tests

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -9,7 +9,29 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual("fixme", items[0].name)
+
+    def test_aged_brie(self):
+        items = [Item("Aged Brie", 1, 2)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 0)
+    
+    def test_sulfuras(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 2, 5)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 100)
+
+    def test_backstage_passes(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 3, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 0)
+
+    
+
+
 
 
 if __name__ == '__main__':
